ede012d0a1cce722b5f2bbbd3e3721ffd47eb629
Crinc <32513174+Cr1nc@users.noreply.github.com>
2019-10-20 00:18:49 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,7 +1,6 @@
 import os
 
-os.system("sudo apt-get install python-pip")
+os.system("sudo apt install python-pip")
 os.system("sudo pip install colored")
-os.system("sudo pip install socket")
 print("\033[1;31;49m")
-print "FINISHED BRUH"
+print "Finished install"
