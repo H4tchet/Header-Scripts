3bf65238641bef38e669e5cc61655813ccd21173
Crinc <32513174+Cr1nc@users.noreply.github.com>
2019-10-20 01:04:42 +0100

    Create verboseheaders.py

---
+++ verboseheaders.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+import colored
+
+headers = sys.argv[1]
+response = requests.get(headers)
+response.headers
+
+#print response.headers
+print("\033[1;32;49m")
+print "\n Verbose Headers:"
+
+if "Server" in response.headers:
+        print("\033[1;31;49m")
+        print response.headers['Server']
+if "X-Powered-By" in response.headers:
+        print response.headers['X-Powered-By']
+if "X-AspNet-Version" in response.headers:
+        print response.headers['X-AspNet-Version']
+if "X-AspNetMvc-Version" in response.headers:
+        print response.headers['X-AspNetMvc-Version']
+else:
+        print("\033[1;32;49m")
+        print "\n [*] Completed - Please enumerate software versions"
