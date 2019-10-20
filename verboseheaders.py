983b46b0cd06935da8e4949559801b289a524630
Crinc <32513174+Cr1nc@users.noreply.github.com>
2019-10-20 01:29:25 +0100

    Update verboseheaders.py

--- verboseheaders.py
+++ verboseheaders.py
@@ -11,14 +11,21 @@ print("\033[1;32;49m")
 print "\n Verbose Headers:"
 
 if "Server" in response.headers:
-        print("\033[1;31;49m")
-        print response.headers['Server']
+	print("\033[1;31;49m")
+	print "Server:"
+	print response.headers['Server']
+
 if "X-Powered-By" in response.headers:
-        print response.headers['X-Powered-By']
+	print "X-Powered-By:"
+	print response.headers['X-Powered-By']
+
 if "X-AspNet-Version" in response.headers:
-        print response.headers['X-AspNet-Version']
+	print "X-AspNet-Version"
+	print response.headers['X-AspNet-Version']
+
 if "X-AspNetMvc-Version" in response.headers:
-        print response.headers['X-AspNetMvc-Version']
+	print "X-AspNetMvc-Version"
+	print response.headers['X-AspNetMvc-Version']
 else:
-        print("\033[1;32;49m")
-        print "\n [*] Completed - Please enumerate software versions"
+	print("\033[1;32;49m")
+	print "\n [*] Completed - Please enumerate more"
